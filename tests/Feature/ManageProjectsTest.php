7852cf9a55ac198f86678bd27ae99deb020a38d3
Javier Gómez <javiergomezve@gmail.com>
2019-11-04 14:17:07 -0500

    [refactor] Guest manage project test

--- tests/Feature/ManageProjectsTest.php
+++ tests/Feature/ManageProjectsTest.php
@@ -6,7 +6,7 @@ use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 
-class ProjectsTest extends TestCase
+class ManageProjectsTest extends TestCase
 {
 
     use WithFaker, RefreshDatabase;
@@ -14,28 +14,12 @@ class ProjectsTest extends TestCase
     /**
      * @test
      */
-    public function guests_cannot_create_projects()
-    {
-        $attributes = factory('App\Project')->raw();
-
-        $this->post('/projects', $attributes)->assertRedirect('login');
-    }
-
-    /**
-     * @test
-     */
-    public function guests_cannot_not_view_projects()
-    {
-        $this->get('/projects')->assertRedirect('login');
-    }
-
-    /**
-     * @test
-     */
-    public function guests_cannot_not_view_a_single_projects()
+    public function guests_cannot_manage_projects()
     {
         $project = factory('App\Project')->create();
 
+        $this->get('/projects')->assertRedirect('login');
+        $this->post('/projects', $project->toArray())->assertRedirect('login');
         $this->get($project->path())->assertRedirect('login');
     }
 
